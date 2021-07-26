46b49537539c6c143d1643bf19e72768bc27a627
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-25 21:42:13 -0600

    update schema.sql, seeds.sql with data

--- db/schema.sql
+++ db/schema.sql
@@ -14,12 +14,15 @@ CREATE TABLE department (
 CREATE TABLE employee_role (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL
+    salary DECIMAL,
+    dept_id INTEGER
 );
 
 
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
-    last_name VARCHAR(30)
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER
 );
