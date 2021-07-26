9d93162cbeca4a39344fe203a790f6311eba7bb1
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-25 22:18:25 -0600

    schema foreign key in employee_role, employee

--- db/schema.sql
+++ db/schema.sql
@@ -7,22 +7,25 @@ DROP TABLE IF EXISTS employee_role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    dept_id INTEGER
+    dept_id INTEGER,
+    FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES employee_role(id)
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
